9730b20c4e8efafbc5aa2590c2194ea06dae02c0
raviteja <ravitejaraviteja666@gmail.com>
2026-04-13 11:12:27 +0530

    fix api url for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
-});
+baseURL: import.meta.env.VITE_API_URL || "https://app-store-backend-dbci.onrender.com",
+    });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
