0693b2b5850ea52ebefd83d6708f952e6c5b0d12
raviteja <ravitejaraviteja666@gmail.com>
2026-04-13 20:52:21 +0530

    fix(frontend): add missing routes for /home and /admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Register from "./pages/Register";
 import AppDetail from "./pages/AppDetail";
 import Profile from "./pages/Profile";
 import Submit from "./pages/Submit";
+import AdminDashboard from "./pages/AdminDashboard";
+import Intro from "./pages/Intro";
 
 const PrivateRoute = ({ children }) => {
   return localStorage.getItem("token") ? children : <Navigate to="/login" />;
@@ -15,11 +17,15 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/intro" element={<Intro />} />
         <Route path="/apps/:id" element={<AppDetail />} />
         <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
         <Route path="/submit" element={<PrivateRoute><Submit /></PrivateRoute>} />
+        <Route path="/admin" element={<PrivateRoute><AdminDashboard /></PrivateRoute>} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
