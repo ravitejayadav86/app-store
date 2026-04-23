dc7deda15e3cdf572593255f81d08139085948bf
raviteja <ravitejaraviteja666@gmail.com>
2026-04-23 19:23:59 +0530

    chore: add capacitor configuration and android project files

---
+++ android/app/src/main/java/com/pandastore/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pandastore.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pandastore.app',
+  appName: 'PandaStore',
+  webDir: 'out'
+};
+
+export default config;
