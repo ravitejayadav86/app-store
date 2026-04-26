84f44ba1b4d4de706d8df55f202c3df088d6322e
raviteja <ravitejaraviteja666@gmail.com>
2026-04-27 00:26:58 +0530

    chore: remove scratch debug scripts

--- scratch/search_apex.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('backend/appstore.db')
-cursor = conn.cursor()
-cursor.execute("SELECT id, name, file_path, developer FROM apps WHERE name LIKE '%Apex%'")
-print(cursor.fetchall())
-conn.close()
